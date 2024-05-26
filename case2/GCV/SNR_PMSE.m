bc3555de5b38f2a3f0276f7a4e97f28254eeb725
roy1025 <70150907+roy1025@users.noreply.github.com>
2024-05-26 21:54:24 +0800

    Add files via upload

---
+++ case2/GCV/SNR_PMSE.m
@@ -0,0 +1,21 @@
+clc;
+clear all;
+path='斑状电荷——电荷_噪声0.5%.csv';%导入excl
+path1='斑状电荷——origin.csv';
+[D,TXT]=xlsread(path);
+[E,TXT1]=xlsread(path1);
+%%
+f_noise = D(:,4)';
+f_origin = E(:,4)';
+h = f_noise-f_origin;
+sum = 0;
+
+for i = 1:1:3600;
+    sum = sum+h(i)^2;
+    i=i+1;
+end
+f_max=max(f_noise);
+
+SNR = -10*log10(sum/(3600*f_max^2));
+PMSE_square = sqrt(sum/(3600*f_max^2));
+
